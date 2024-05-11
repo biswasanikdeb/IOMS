afc53959bc1d610a0195654117cbcd978a428936
Anik Deb Biswas <biswasanikdeb46@gmail.com>
2024-05-11 21:44:33 +0600

    modifying data is working fine

--- src/main/java/com/newCommon/DataManagement.java
+++ src/main/java/com/newCommon/DataManagement.java
@@ -1,27 +1,19 @@
 package com.newcommon;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.lang.Exception;
 
-
-
 public class DataManagement {
     private String data[];
-    File newfile;
-    
 
     public void addData(String data1, String data2, File newFile) { // add data to file by tab and new line) {
 
-       
-        
         try {
             newFile.createNewFile();
-            FileWriter writer = new FileWriter(newFile,true);
+            FileWriter writer = new FileWriter(newFile, true);
             writer.write(data1 + "\t" + data2 + "\n");
             writer.flush();
             writer.close();
@@ -30,42 +22,38 @@ public class DataManagement {
         }
     }
 
-    public String[] readData(File newFile, Scanner sc) { //reads data by line, has to use in a loop
-        
+    public String[] readData(File newFile, Scanner sc) { // reads data by line, has to use in a loop
+
         try {
-            
-            String tempData= sc.nextLine();
+
+            String tempData = sc.nextLine();
             data = tempData.split("\t");
-            
-            
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         return data;
     }
-    public void modifyData(String newData, String OldData, File Datafile)throws IOException{
+
+    public void modifyData(String newData, String OldData, File Datafile, Scanner sc) throws IOException {
         File tmpfile = new File("./tempfile.txt");
-        
-        BufferedReader reader = new BufferedReader(new FileReader(Datafile));
         FileWriter writer = new FileWriter(tmpfile);
-       
 
-        String line ;
-        while ((line =reader.readLine()) != null) {
-            line.replaceAll(OldData, newData);
-            writer.write(line+ System.lineSeparator());
-            
+        while (sc.hasNextLine()) {
+            data = readData(Datafile, sc);
+            if (data[0].equals(OldData)) {
+                data[0] = newData;
+            }
+            if (data[1].equals(OldData)) {
+                data[1] = newData;
+            }
+            addData(data[0], data[1], tmpfile);
         }
-        reader.close();
+        sc.close();
         writer.close();
-        tmpfile.renameTo(Datafile);
         Datafile.delete();
-        
-       
-
-
+        tmpfile.renameTo(Datafile);
 
     }
 
