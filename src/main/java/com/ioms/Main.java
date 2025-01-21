e079ff24d8ab3d96b0b5d9ea681059d41c81ac2e
Anik Deb Biswas <biswasanikdeb46@gmail.com>
2025-01-21 12:57:07 +0600

    modified

--- src/main/java/com/ioms/Main.java
+++ src/main/java/com/ioms/Main.java
@@ -1,6 +1,6 @@
 package com.ioms;
 
-import com.GUI.LoginPage;
+import com.GUI.*;
 
 
 public class Main  {
@@ -11,6 +11,7 @@ public class Main  {
         LoginPage obj1 = new LoginPage();
         //TabbedOrder obj1 = new TabbedOrder();
         //ClientPage obj1 = new ClientPage(null);
+        //CustomerInfo obj1 = new CustomerInfo(1);
         obj1.setVisible(true);
     }
 }
